0f94f9c4fe72bb91d077243f112dd412c221edb5
Лев <lev@poznanin.ru>
2022-07-19 15:20:42 +0300

    Добавлено настройки копирования артефакта web-app

--- kubernetes/dz1/create_image.bat
+++ kubernetes/dz1/create_image.bat
@@ -1,1 +1,4 @@
-docker build --build-arg JAR_FILE=.\web-app\build\libs\*.jar -t spring-boot-docker:latest .
+SET SRC=..\..\web-app\build\libs\web-app.jar
+copy %SRC% web-app.jar
+docker build --build-arg JAR_FILE=web-app.jar -t arch.homework.health.dz1:1.0 .
+del web-app.jar
