74e226e63978081f43d50bd4b366c15e5d8c7852
Лев <lev@poznanin.ru>
2022-07-19 16:54:36 +0300

    Изменено название образа для публикации в докерхабе

--- kubernetes/dz1/create_image.bat
+++ kubernetes/dz1/create_image.bat
@@ -1,4 +1,4 @@
 SET SRC=..\..\web-app\build\libs\web-app.jar
 copy %SRC% web-app.jar
-docker build --build-arg JAR_FILE=web-app.jar -t arch.homework.health.dz1:1.0 .
+docker build --build-arg JAR_FILE=web-app.jar -t poznaninlp/otus_homework:1.0 .
 del web-app.jar
