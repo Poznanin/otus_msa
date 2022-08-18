7d73379b876c9e77213300baad29a2772a9b133a
Лев <lev@poznanin.ru>
2022-08-18 18:20:55 +0300

    Добавлены Entity для User

--- web-app/src/main/java/arch/homework/webapp/dao/entity/User.java
+++ web-app/src/main/java/arch/homework/webapp/dao/entity/User.java
@@ -1,4 +1,46 @@
 package arch.homework.webapp.dao.entity;
 
+import lombok.*;
+import org.springframework.lang.Nullable;
+
+import javax.persistence.*;
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity
+@Table(name = "user", schema = "public")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(doNotUseGetters = true, onlyExplicitlyIncluded = true)
+@ToString(doNotUseGetters = true)
 public class User {
+    @Id
+    @Column(name = "id", updatable = false, nullable = false)
+    @EqualsAndHashCode.Include
+    private UUID id;
+
+    @Column(name = "name",nullable = false)
+    private String name;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "mail")
+    private String mail;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @PrePersist
+    protected void onCreate() {
+        if (Objects.isNull(this.id)) {
+            this.id = UUID.randomUUID();
+        }
+    }
+
 }
---
+++ web-app/src/main/java/arch/homework/webapp/dao/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package arch.homework.webapp.dao.repository;
+
+import arch.homework.webapp.dao.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
