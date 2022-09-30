44be2f207a043e364de545091450c5f7b775fa17
Лев <lev@poznanin.ru>
2022-09-30 16:47:22 +0300

    Добавлены Helm для конфигурирования БД, сервисов, деплоев, мониторинга

--- kubernetes/dz2/create_image.bat
+++ kubernetes/dz2/create_image.bat
@@ -1,4 +1,4 @@
 SET SRC=..\..\web-app\build\libs\web-app.jar
 copy %SRC% web-app.jar
-docker build --build-arg JAR_FILE=web-app.jar -t poznaninlp/otus_homework:1.2.4 .
+docker build --build-arg JAR_FILE=web-app.jar -t poznaninlp/otus_homework:1.2.z .
 del web-app.jar
